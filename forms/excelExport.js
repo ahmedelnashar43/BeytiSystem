22607a5a3fa9f1b262b72cdc424f0a5802d98675
ahmedelnashar43 <ahmedelnashar2016@gmail.com>
2024-10-01 16:18:36 +0300

    Update excelExport.js

--- forms/excelExport.js
+++ forms/excelExport.js
@@ -1,10 +1,27 @@
-// excelExport.js
+import * as XLSX from 'xlsx';
+
 function exportToExcel(data) {
-    const worksheet = XLSX.utils.json_to_sheet(data);
-    const workbook = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(workbook, worksheet, "Lab Data");
-    XLSX.writeFile(workbook, "lab_data.xlsx");
+  // Convert Date objects to strings in the format that Excel can recognize
+  const processedData = data.map(row => ({
+    ...row,
+    timestamp: row.timestamp instanceof Date ? row.timestamp.toISOString() : row.timestamp
+  }));
+
+  const worksheet = XLSX.utils.json_to_sheet(processedData);
+
+  // Format the timestamp column as a date in Excel
+  const range = XLSX.utils.decode_range(worksheet['!ref']);
+  const timestampCol = Object.keys(processedData[0]).indexOf('timestamp');
+  for (let row = range.s.r + 1; row <= range.e.r; ++row) {
+    const cell = XLSX.utils.encode_cell({r: row, c: timestampCol});
+    if (worksheet[cell]) {
+      worksheet[cell].z = XLSX.SSF._table[14];  // Date format: dd-mmm-yy
+    }
   }
-  
-  export { exportToExcel }; // Export the function for use in other files
-  
+
+  const workbook = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(workbook, worksheet, "Lab Data");
+  XLSX.writeFile(workbook, "lab_data.xlsx");
+}
+
+export { exportToExcel };
